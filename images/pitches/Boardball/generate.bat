4156b19c4a8c4e5a58ca6e4740f4aeff97ee200d
Szieberth Ádám <sziebadam@gmail.com>
2017-03-14 05:56:54 +0100

    pitches updated

--- images/pitches/Boardball/generate.bat
+++ images/pitches/Boardball/generate.bat
@@ -6,8 +6,9 @@ convert -size 782x452 tile:%%f png24:%%~nf.png
 for %%l in (Layers\*) do (
 for /F "tokens=1,2,3 delims=&" %%a in ("%%~nl") do (
 echo    adding layer %%a with transparency 1/%%b and mode %%c...
-convert %%~nf.png ^( %%l -alpha set -channel a -evaluate divide %%b ^) -compose %%c -composite -type palette -colors 255 %%~nf.png
+convert %%~nf.png ^( %%l -alpha set -channel a -evaluate divide %%b ^) -compose %%c -composite %%~nf.png
 )
+convert %%~nf.png -crop 780x450+0+0 +repage -type palette -colors 255 %%~nf.png
 )
 echo Done.
 )
